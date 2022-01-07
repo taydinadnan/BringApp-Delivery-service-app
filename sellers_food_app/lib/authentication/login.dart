38ea046643a7495b73dd8a0b9d51c011759ef29a
Turgay Aydin <taydinadnan@gmail.com>
2022-01-07 13:14:16 +0100

    seller register,login foms

--- sellers_food_app/lib/authentication/login.dart
+++ sellers_food_app/lib/authentication/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sellers_food_app/widgets/custom_text_field.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -8,8 +9,77 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Container(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.all(15),
+              child: Image.asset(
+                'images/seller1.png',
+                height: 270,
+              ),
+            ),
+          ),
+          const Center(
+            child: Text(
+              'Sellers Login',
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.orange,
+                fontFamily: "Lobster",
+              ),
+            ),
+          ),
+          Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                CustomTextField(
+                  data: Icons.email,
+                  controller: passwordController,
+                  hintText: "Email",
+                  isObsecre: false,
+                ),
+                CustomTextField(
+                  data: Icons.lock,
+                  controller: emailController,
+                  hintText: "Password",
+                  isObsecre: true,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 30),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.orange,
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 50,
+                        vertical: 10,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
--- sellers_food_app/lib/authentication/register.dart
+++ sellers_food_app/lib/authentication/register.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:sellers_food_app/widgets/custom_text_field.dart';
 
 class RegisterScreen extends StatefulWidget {
@@ -9,26 +13,148 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
-  TextEditingController anyController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmpasswordController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController locationController = TextEditingController();
+
+  XFile? imageXFile;
+  final ImagePicker _picker = ImagePicker();
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        CustomTextField(
-          controller: anyController,
-          data: Icons.phone,
-          hintText: "Username",
-          isObsecre: false,
-          enabled: true,
+    return SingleChildScrollView(
+      child: Container(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            const SizedBox(height: 10),
+            InkWell(
+              child: CircleAvatar(
+                radius: MediaQuery.of(context).size.width * 0.20,
+                backgroundColor: Colors.white,
+                backgroundImage: imageXFile == null
+                    ? null
+                    : FileImage(
+                        File(imageXFile!.path),
+                      ),
+                child: imageXFile == null
+                    ? Icon(
+                        Icons.add_photo_alternate,
+                        size: MediaQuery.of(context).size.width * 0.20,
+                        color: Colors.grey,
+                      )
+                    : null,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  CustomTextField(
+                    data: Icons.person,
+                    controller: nameController,
+                    hintText: "Name",
+                    isObsecre: false,
+                  ),
+                  CustomTextField(
+                    data: Icons.email,
+                    controller: emailController,
+                    hintText: "Email",
+                    isObsecre: false,
+                  ),
+                  CustomTextField(
+                    data: Icons.lock,
+                    controller: passwordController,
+                    hintText: "Password",
+                    isObsecre: true,
+                  ),
+                  CustomTextField(
+                    data: Icons.lock,
+                    controller: confirmpasswordController,
+                    hintText: "Confirm password",
+                    isObsecre: true,
+                  ),
+                  CustomTextField(
+                    data: Icons.phone_android_outlined,
+                    controller: phoneController,
+                    hintText: "Phone nummber",
+                    isObsecre: false,
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        width: 300,
+                        child: CustomTextField(
+                          data: Icons.my_location,
+                          controller: locationController,
+                          hintText: "Cafe/Restorant Address",
+                          isObsecre: false,
+                          enabled: false,
+                        ),
+                      ),
+                      Center(
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.location_on,
+                            size: 40,
+                          ),
+                          color: Colors.red,
+                        ),
+                      )
+                    ],
+                  ),
+                  // Container(
+                  //   width: 60,
+                  //   height: 50,
+                  //   alignment: Alignment.center,
+                  //   child: ElevatedButton.icon(
+                  //     onPressed: () {},
+                  //     icon: const Icon(Icons.location_history),
+                  //     label: const Text(
+                  //       "",
+                  //       style: TextStyle(color: Colors.white),
+                  //     ),
+                  //     style: ElevatedButton.styleFrom(
+                  //       primary: Colors.orange,
+                  //       shape: RoundedRectangleBorder(
+                  //         borderRadius: BorderRadius.circular(30),
+                  //       ),
+                  //     ),
+                  //   ),
+                  // )
+                ],
+              ),
+            ),
+            const SizedBox(height: 30),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 30),
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text(
+                  'Sign Up',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.orange,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 50,
+                    vertical: 10,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
-        CustomTextField(
-          controller: anyController,
-          data: Icons.phone,
-          hintText: "Password",
-          isObsecre: true,
-          enabled: true,
-        ),
-      ],
+      ),
     );
   }
 }
