ef6e83a55e18cb13f649f5ec5ef689440fa6b91f
Turgay Aydin <taydinadnan@gmail.com>
2022-01-24 13:43:26 +0100

    retrieve & display user cart items with quantity numbers

--- users_food_app/lib/screens/cart_screen.dart
+++ users_food_app/lib/screens/cart_screen.dart
@@ -1,6 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:users_food_app/assistantMethods/assistant_methods.dart';
 import 'package:users_food_app/widgets/app_bar.dart';
+import 'package:users_food_app/widgets/cart_item_design.dart';
+import 'package:users_food_app/widgets/progress_bar.dart';
+
+import '../models/items.dart';
+import '../widgets/text_widget_header.dart';
 
 class CartScreen extends StatefulWidget {
   final String? sellerUID;
@@ -12,11 +18,13 @@ class CartScreen extends StatefulWidget {
 }
 
 class _CartScreenState extends State<CartScreen> {
+  List<int>? separateItemQuantityList;
+
   @override
   void initState() {
     super.initState();
 
-    separateItemQuantities();
+    separateItemQuantityList = separateItemQuantities();
   }
 
   @override
@@ -26,31 +34,95 @@ class _CartScreenState extends State<CartScreen> {
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          const SizedBox(width: 10),
+          const SizedBox(
+            width: 10,
+          ),
           Align(
             alignment: Alignment.bottomLeft,
             child: FloatingActionButton.extended(
-              onPressed: () {},
+              heroTag: "btn1",
               label: const Text(
                 "Clear Cart",
                 style: TextStyle(fontSize: 16),
               ),
-              backgroundColor: Colors.orangeAccent,
+              backgroundColor: Colors.cyan,
               icon: const Icon(Icons.clear_all),
+              onPressed: () {
+                // clearCartNow(context);
+
+                // Navigator.push(context, MaterialPageRoute(builder: (c)=> const MySplashScreen()));
+
+                // Fluttertoast.showToast(msg: "Cart has been cleared.");
+              },
             ),
           ),
           Align(
-            alignment: Alignment.bottomRight,
+            alignment: Alignment.bottomLeft,
             child: FloatingActionButton.extended(
-              onPressed: () {},
+              heroTag: "btn2",
               label: const Text(
                 "Check Out",
                 style: TextStyle(fontSize: 16),
               ),
-              backgroundColor: Colors.orangeAccent,
+              backgroundColor: Colors.cyan,
               icon: const Icon(Icons.navigate_next),
+              onPressed: () {
+                // Navigator.push(
+                //     context,
+                //     MaterialPageRoute(
+                //         builder: (c)=> AddressScreen(
+                //           totalAmount: totalAmount.toDouble(),
+                //           sellerUID: widget.sellerUID,
+                //         ),
+                //     ),
+                // );
+              },
             ),
-          )
+          ),
+        ],
+      ),
+      body: CustomScrollView(
+        slivers: [
+          //overall total price
+          SliverPersistentHeader(
+              pinned: true, delegate: TextWidgetHeader(title: "My Cart List")),
+
+          //display cart items with quantity numbers
+          StreamBuilder<QuerySnapshot>(
+            stream: FirebaseFirestore.instance
+                .collection("items")
+                .where("itemID", whereIn: separateItemIDs())
+                .orderBy("publishedDate", descending: true)
+                .snapshots(),
+            builder: (context, snapshot) {
+              return !snapshot.hasData
+                  ? SliverToBoxAdapter(
+                      child: Center(
+                        child: circularProgress(),
+                      ),
+                    )
+                  : snapshot.data!.docs.length == 0
+                      ? Container()
+                      : SliverList(
+                          delegate: SliverChildBuilderDelegate(
+                            (context, index) {
+                              Items model = Items.fromJson(
+                                snapshot.data!.docs[index].data()!
+                                    as Map<String, dynamic>,
+                              );
+                              return CartItemDesign(
+                                model: model,
+                                context: context,
+                                quanNumber: separateItemQuantityList![index],
+                              );
+                            },
+                            childCount: snapshot.hasData
+                                ? snapshot.data!.docs.length
+                                : 0,
+                          ),
+                        );
+            },
+          ),
         ],
       ),
     );
