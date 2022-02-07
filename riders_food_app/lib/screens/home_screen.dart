5a6f8c30fcb41a751b255beaa6a6d74479a4318f
Turgay Aydin <taydinadnan@gmail.com>
2022-02-07 15:19:18 +0100

    Rider Dashboard UI , GridView Cards

--- riders_food_app/lib/screens/home_screen.dart
+++ riders_food_app/lib/screens/home_screen.dart
@@ -13,6 +13,105 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final TextEditingController _controller = TextEditingController();
 
+  Card makeDashboardItem(String title, IconData iconData, int index) {
+    return Card(
+      elevation: 2,
+      margin: const EdgeInsets.all(8),
+      child: Container(
+        decoration: index == 0 || index == 3 || index == 4
+            ? BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                gradient: const LinearGradient(
+                  begin: FractionalOffset(0.0, 0.0),
+                  end: FractionalOffset(3.0, -1.0),
+                  colors: [
+                    Color(0xFF004B8D),
+                    Color(0xFFffffff),
+                  ],
+                ),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.grey,
+                    blurRadius: 3,
+                    offset: Offset(4, 3),
+                  )
+                ],
+              )
+            : BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                gradient: const LinearGradient(
+                  begin: FractionalOffset(0.0, 0.0),
+                  end: FractionalOffset(3.0, -1.0),
+                  colors: [
+                    Colors.cyan,
+                    Colors.amber,
+                  ],
+                ),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.grey,
+                    blurRadius: 3,
+                    offset: Offset(4, 3),
+                  )
+                ],
+              ),
+        child: InkWell(
+          onTap: () {
+            if (index == 0) {
+              //new orders
+            }
+            if (index == 1) {
+              //Parcels in progress
+            }
+            if (index == 2) {
+              //not yet delivered
+            }
+            if (index == 3) {
+              //history
+            }
+            if (index == 4) {
+              //total earnings
+            }
+            if (index == 5) {
+              //logout
+              firebaseAuth.signOut().then((value) {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: ((context) => const AuthScreen())));
+              });
+            }
+          },
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.min,
+            verticalDirection: VerticalDirection.down,
+            children: [
+              const SizedBox(height: 50),
+              Center(
+                child: Icon(
+                  iconData,
+                  size: 40,
+                  color: Colors.black,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Center(
+                child: Text(
+                  title,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,24 +128,31 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ),
         ),
-        title: Text(sharedPreferences!.getString("name")!),
+        title: Text(
+          "Welcome " + sharedPreferences!.getString("name")!,
+          style: const TextStyle(
+            fontSize: 40,
+            color: Colors.white,
+            fontFamily: "Signatra",
+            letterSpacing: 2,
+          ),
+        ),
         centerTitle: true,
         automaticallyImplyLeading: false,
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            firebaseAuth.signOut().then((value) {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (c) => const AuthScreen(),
-                ),
-              );
-              _controller.clear();
-            });
-          },
-          child: const Text("Logout"),
+      body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 1),
+        child: GridView.count(
+          crossAxisCount: 2,
+          padding: const EdgeInsets.all(2),
+          children: [
+            makeDashboardItem("New Available", Icons.assignment, 0),
+            makeDashboardItem("Parcels in Progress", Icons.airport_shuttle, 1),
+            makeDashboardItem("Not Yet Delivered", Icons.location_history, 2),
+            makeDashboardItem("History", Icons.done_all, 3),
+            makeDashboardItem("Total Earnings", Icons.monetization_on, 4),
+            makeDashboardItem("Logout", Icons.logout, 5),
+          ],
         ),
       ),
     );
