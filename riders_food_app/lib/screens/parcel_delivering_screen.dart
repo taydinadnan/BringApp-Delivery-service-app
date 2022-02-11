aeb77676bd852c7d993fd8453b1a5068997f18e4
Turgay Aydin <taydinadnan@gmail.com>
2022-02-11 13:50:42 +0100

    order delivered method

--- riders_food_app/lib/screens/parcel_delivering_screen.dart
+++ riders_food_app/lib/screens/parcel_delivering_screen.dart
@@ -1,4 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:riders_food_app/splash_screen/splash_screen.dart';
+
+import '../assistantMethods/get_current_location.dart';
+import '../global/global.dart';
+import '../maps/map_utils.dart';
 
 class ParcelDeliveringScreen extends StatefulWidget {
   String? purchaserId;
@@ -21,8 +27,137 @@ class ParcelDeliveringScreen extends StatefulWidget {
 }
 
 class _ParcelDeliveringScreenState extends State<ParcelDeliveringScreen> {
+  // updating status and comfirming order has picked
+  confirmParcelHasBeenDelivered(getOrderId, sellerId, purchaserId,
+      purchaserAddress, purchaserLat, purchaserLng) {
+    FirebaseFirestore.instance.collection("orders").doc(getOrderId).update({
+      "status": "ended",
+      "address": completeAddress,
+      "lat": position!.latitude,
+      "lng": position!.longitude,
+      "earnings": "", //pay per delivery
+    }).then((value) {
+      FirebaseFirestore.instance
+          .collection("riders")
+          .doc(sharedPreferences!.getString("uid"))
+          .update({
+        "earnings": "", //total earnings of rider
+      });
+    }).then((value) {
+      FirebaseFirestore.instance
+          .collection("sellers")
+          .doc(widget.sellerId)
+          .update({
+        "earnings": "", // total earnings of seller
+      });
+    }).then((value) {
+      FirebaseFirestore.instance
+          .collection("users")
+          .doc(purchaserId)
+          .collection("orders")
+          .doc(getOrderId)
+          .update({
+        "status": "ended",
+        "riderUID": sharedPreferences!.getString("uid"),
+      });
+    });
+
+    Navigator.push(
+        context, MaterialPageRoute(builder: (c) => const SplashScreen()));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            "images/confirm1.png",
+            width: 350,
+          ),
+          const SizedBox(height: 5),
+          GestureDetector(
+            onTap: () {
+              //TODO: show location from rider current location towards seller location
+              MapUtils.launchMapFromSourceToDestination(
+                  position!.latitude,
+                  position!.longitude,
+                  widget.purchaserLat,
+                  widget.purchaserLng);
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  "images/restaurant.png",
+                  width: 50,
+                ),
+                const SizedBox(width: 7),
+                Column(
+                  children: const [
+                    SizedBox(height: 12),
+                    Text(
+                      "Show Cafe/Restaurant Location",
+                      style: TextStyle(
+                        fontFamily: "Signatra",
+                        fontSize: 20,
+                        letterSpacing: 2,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+          const SizedBox(height: 40),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Center(
+              child: InkWell(
+                onTap: () {
+                  //TODO: confirm that rider has picked order
+                  //rider location updates
+                  UserLocation uLocation = UserLocation();
+                  uLocation.getCurrenLocation();
+
+                  confirmParcelHasBeenDelivered(
+                    widget.getOrderId,
+                    widget.sellerId,
+                    widget.purchaserId,
+                    widget.purchaserAddress,
+                    widget.purchaserLat,
+                    widget.purchaserLng,
+                  );
+                },
+                child: Container(
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: FractionalOffset(0.0, 0.0),
+                      end: FractionalOffset(3.0, -1.0),
+                      colors: [
+                        Color(0xFF004B8D),
+                        Color(0xFFffffff),
+                      ],
+                    ),
+                  ),
+                  width: MediaQuery.of(context).size.width - 90,
+                  height: 50,
+                  child: const Center(
+                      child: Text(
+                    "Delivered",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                    ),
+                  )),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
