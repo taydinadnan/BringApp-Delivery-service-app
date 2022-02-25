65060024c36e1120b113ac0f4d17dc8ae4690a21
Turgay Aydin <taydinadnan@gmail.com>
2022-02-25 15:47:16 +0100

    Home screen added

--- bringapp_admin_web_portal/lib/main.dart
+++ bringapp_admin_web_portal/lib/main.dart
@@ -11,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Admin Web Portal',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
--- bringapp_admin_web_portal/lib/screens/home_screen.dart
+++ bringapp_admin_web_portal/lib/screens/home_screen.dart
@@ -10,6 +10,10 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Welcome to Admin Web Portal"),
+      ),
+    );
   }
 }
