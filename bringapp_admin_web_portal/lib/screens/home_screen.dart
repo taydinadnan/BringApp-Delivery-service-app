0ca7fc6a918159036c43f48475f559c9aec95f0f
Turgay Aydin <taydinadnan@gmail.com>
2022-02-26 12:36:31 +0100

    Live time,date,seconds

--- bringapp_admin_web_portal/lib/screens/home_screen.dart
+++ bringapp_admin_web_portal/lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,11 +11,68 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String timeText = '';
+  String dateText = '';
+
+  //getting time
+  String formatCurrentLiveTime(DateTime time) {
+    return DateFormat("hh:mm:ss a").format(time);
+  }
+
+  String formatCurrentDate(DateTime date) {
+    return DateFormat("dd MMMM, yyyy").format(date);
+  }
+
+  getCurrentLiveTime() {
+    final DateTime timeNow = DateTime.now();
+    final String liveTime = formatCurrentLiveTime(timeNow);
+    final String liveDate = formatCurrentDate(timeNow);
+
+    if (this.mounted) {
+      setState(() {
+        timeText = liveTime;
+        dateText = liveDate;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    //time
+    timeText = formatCurrentLiveTime(DateTime.now());
+    //date
+    dateText = formatCurrentDate(DateTime.now());
+
+    //seconds
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      getCurrentLiveTime();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Welcome to Admin Web Portal"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Text(
+                timeText + "\n" + dateText,
+                style: const TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                  letterSpacing: 3,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
